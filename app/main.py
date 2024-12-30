276c0b9d5c0d09124bb91ea8e0331fdc8ea3e2be
thtv231 <126608842+thtv231@users.noreply.github.com>
2024-12-30 19:33:35 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 def get_clean_data():
-  data = pd.read_csv("../data/data.csv")
+  data = pd.read_csv("data/data.csv")
   
   data = data.drop(['Unnamed: 32', 'id'], axis=1)
   
@@ -139,8 +139,8 @@ def get_radar_chart(input_data):
 
 
 def add_predictions(input_data):
-  model = pickle.load(open("../model/model.pkl", "rb"))
-  scaler = pickle.load(open("../model/scaler.pkl", "rb"))
+  model = pickle.load(open("model/model.pkl", "rb"))
+  scaler = pickle.load(open("model/scaler.pkl", "rb"))
   
   input_array = np.array(list(input_data.values())).reshape(1, -1)
   
@@ -172,7 +172,7 @@ def main():
     initial_sidebar_state="expanded"
   )
   
-  with open("../assets/style.css") as f:
+  with open("assets/style.css") as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
   
   input_data = add_sidebar()
